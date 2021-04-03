708d614777b3c628593329e720d186a1804df9c6
bok_suh <>
2021-04-03 07:02:48 +0000

    code done

--- main.c
+++ main.c
@@ -11,20 +11,48 @@ int number[100] = {0, };
 
 int main(void) {
 	int sum = 0;
-	srand(time(NULL));
+	char* string;
   FILE *fp = fopen("result.dat", "w");
 
+	for (int i=0; i<10; i++) {
+		if (fork() == 0) {
+			srand(time(NULL));
+			for(int i=0; i<100000; i++) {
+				number[rand()%100]++;
+			}
+			sprintf(string, "[Pid: %d] --> ", getpid());
+			fputs(string, fp);
 
+			for (int i=0; i<100; i++) {
+				sprintf(string, "%d ", number[i]);
+				fputs(string, fp);
+				sum += number[i];
+			}
 
+			sprintf(string, "(Sum = %d)\n", sum);
+			fputs(string, fp);
 
+			exit(0);
+		}
+	}
 
+	for (int i=0; i<10; i++) {
+		wait(NULL);
+	}
 
+	sprintf(string, "Main Process --> ");
+	fputs(string, fp);
 
+	for (int i=0; i<100; i++) {
+		sprintf(string, "%d ", number[i]);
+		fputs(string, fp);
+		sum += number[i];
+	}
+
+	sprintf(string, "(Sum = %d)\n", sum);
+	fputs(string, fp);
 
 	fclose(fp);
-	// 0~100 rand()%100
-	
-	
-	
+
 	return 0;
 }
